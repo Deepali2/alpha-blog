22ec456e4c5d4f36488e03bda5146e6083189db7
Deepali Garg <deepali.garg@zennioptical.com>
2020-12-27 08:22:08 -0800

    styles validation and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Gets rid of the div with class 'field_with_errors' that rails auto-creates for error validations
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
