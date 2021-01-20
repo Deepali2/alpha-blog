7426a9b071823e46c9789d2833444e6d01b91e4e
Deepali Garg <deepali.garg@zennioptical.com>
2021-01-20 07:47:01 -0800

    add new users sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new' # users is the controller and new is the action
+  # post 'users', to: 'users#create' or do the following way
+  resources :users, except: [:new]
 end
